109db0262007b5552beb634a56a0184f0fff3b94
mohd sofi <mohdsofi@123>
2023-01-16 15:00:38 +0530

    inital commit

---
+++ square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+list = [2,4,6,8,10]
+print("square of list")
+for i in list:
+    print(i**2)
+print("cube of list")
+for i in list:
+    print(i**3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
